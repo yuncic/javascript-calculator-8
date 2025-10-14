ec4fffc1649550b79af9c12b262079baf4167f0a
김상윤 <ksy151020@gmail.com>
2025-10-14 23:45:59 +0900

    feat: 기본 구분자 처리

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const STRING_INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const INPUT_RESULT = STRING_INPUT.split(/,|:/).map(Number);
     if (STRING_INPUT === '') {
       console.log('결과 : 0');
+    }else{
+      let sum = 0;
+      for (let i = 0; i < INPUT_RESULT.length; i++) {
+        sum += INPUT_RESULT[i];
+      }
+      console.log(sum)
     }
   }
 }
 
+export default App;
