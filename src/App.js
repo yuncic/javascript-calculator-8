cab8aa8f88de70c41181fe0336773cdcdc6edc18
김상윤 <ksy151020@gmail.com>
2025-10-14 23:25:07 +0900

    feat: 문자열 입력받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console } from '@woowacourse/mission-utils';
+
+
 class App {
-  async run() {}
+  async run() {
+    const STRING_INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    if (STRING_INPUT === '') {
+      console.log('결과 : 0');
+    }
+  }
 }
 
-export default App;
