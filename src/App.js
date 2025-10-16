c7cb547e149a2b7986f52430938f232a4a4f8798
김상윤 <ksy151020@gmail.com>
2025-10-16 21:23:06 +0900

    feat: 커스텀 구분자 처리 및 기본 구분자 처리 코드 수정

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const STRING_INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
-    const INPUT_RESULT = STRING_INPUT.split(/,|:/).map(Number);
+    
+    //빈 문자열 처리
     if (STRING_INPUT === '') {
       console.log('결과 : 0');
-    }else{
-      let sum = 0;
-      for (let i = 0; i < INPUT_RESULT.length; i++) {
-        sum += INPUT_RESULT[i];
-      }
-      console.log(sum)
+      return;
     }
+
+    let NUMBERS = [];
+    let DELIMITER_PART ='';
+    let NUMBER_PART = '';
+
+    //커스텀 구분자 처리
+    if (STRING_INPUT.startsWith('//')) {
+      // '\n' 기준으로 커스텀 구분자와 본문 분리
+      [DELIMITER_PART, NUMBER_PART] = STRING_INPUT.split('\\n');
+      const CUSTOM_DELIMITER = DELIMITER_PART.replace('//','');
+      // 커스텀 구분자로 분리
+      NUMBERS = NUMBER_PART.split(CUSTOM_DELIMITER).map(Number);
+
+    }else{
+      // 기본 구분자
+      NUMBERS = STRING_INPUT.split(/,|:/).map(Number);
+    }
+    //합산
+    let result = 0;
+    for (let i = 0; i < NUMBERS.length; i++){
+      result += NUMBERS[i];
+    }
+    console.log(`결과 : ${result}`)
   }
 }
 
