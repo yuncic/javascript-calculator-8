017bbfb2050a2a181e4d961f273decda5be22785
김상윤 <ksy151020@gmail.com>
2025-10-18 20:25:40 +0900

    refactor: Airbnb 자바스크립트 스타일 가이드 기준에 따라 수정

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const STRING_INPUT = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const stingInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
     //빈 문자열 처리
-    if (STRING_INPUT === '') {
+    if (stingInput === '') {
       Console.print('결과 : 0'); // 테스트파일에 mission-utils의 Console.print만 Spy한다고 되어있음 console.log -> Console.print
       return;
     }
 
     // 입력값 시작 검사
-    const FIRST_CHAR = STRING_INPUT[0];
-   
-    if (!STRING_INPUT || (!STRING_INPUT.startsWith('//') && !(FIRST_CHAR >= '0'))) {
-      if(STRING_INPUT.startsWith('-')){
+    if (!stingInput || (!stingInput.startsWith('//') && !(Number(stingInput[0]) >= 0))) {
+      if (stingInput.startsWith('-')){
         throw new Error('[ERROR] 음수는 입력할 수 없습니다.'); //기존의 proccess.exit(1)은 코드를 강제종료 시켜서 에러 throw 하기도 전에 종료됨 -> 테스트 에러
         
       } 
@@ -23,39 +21,39 @@ class App {
       
     }
 
-    let NUMBERS = [];
-    let DELIMITER_PART ='';
-    let NUMBER_PART = '';
+    let numbers = [];
+    let delimiterPart ='';
+    let numberPart = '';
 
     //커스텀 구분자 처리
-    if (STRING_INPUT.startsWith('//')) {
+    if (stingInput.startsWith('//')) {
       // 커스텀 구분자 문법 검사
-      if (!STRING_INPUT.includes('\\n')){
+      if (!stingInput.includes('\\n')){
         throw new Error('[ERROR] 커스텀 구분자 문법을 제대로 입력해주세요.');
       }
       // '\n' 기준으로 커스텀 구분자와 본문 분리
-      [DELIMITER_PART, NUMBER_PART] = STRING_INPUT.split('\\n');
-      const CUSTOM_DELIMITER = DELIMITER_PART.replace('//','');
+      [delimiterPart, numberPart] = stingInput.split('\\n');
+      const CUSTOM_DELIMITER = delimiterPart.replace('//','');
       // 커스텀 구분자로 분리
-      NUMBERS = NUMBER_PART.split(CUSTOM_DELIMITER).map(str => {
+      numbers = numberPart.split(CUSTOM_DELIMITER).map(str => {
         const NUM_STR = str.trim();
         //구분자 다음 숫자가 안 나올때(공백처리됨)
         if (NUM_STR === ''){
-          throw new Error("[ERROR] 제대로된 구분자를 사용해주세요.");   
+          throw new Error("[ERROR] 제대로된 구분자를 사용해주세요");   
         }
 
         const NUM = Number(NUM_STR);
 
         if (isNaN(NUM)) {
-          throw new Error("[ERROR] 제대로된 구분자를 사용해주세요.");          
+          throw new Error("[ERROR] 제대로된 구분자를 사용해주세요");          
         }
 
         if (NUM <0 ) {
-          throw new Error('[ERROR] 음수는 입력할 수 없습니다.')
+          throw new Error('[ERROR] 음수는 입력할 수 없습니다')
         }
 
         if (!Number.isInteger(NUM)) {
-          throw new Error('[ERROR] 정수만 입력 가능합니다.')
+          throw new Error('[ERROR] 정수만 입력 가능합니다')
         }
 
         return NUM;
@@ -64,20 +62,20 @@ class App {
 
     }else{
       // 기본 구분자
-      NUMBERS = STRING_INPUT.split(/,|:/).map(str => {
+      numbers = stingInput.split(/,|:/).map(str => {
         const NUM_STR = str.trim();
         const NUM = Number(NUM_STR);
 
         if (isNaN(NUM)) {
-          throw new Error('[ERROR] 제대로된 구분자를 사용해주세요.')
+          throw new Error('[ERROR] 제대로된 구분자를 사용해주세요')
         }
 
         if (NUM < 0) {
-          throw new Error('[ERROR] 음수는 입력할 수 없습니다.')
+          throw new Error('[ERROR] 음수는 입력할 수 없습니다')
         } 
 
         if (!Number.isInteger(NUM)) {
-          throw new Error('[ERROR] 정수만 입력 가능합니다.')
+          throw new Error('[ERROR] 정수만 입력 가능합니다')
         }
         return NUM;
       });
@@ -86,8 +84,8 @@ class App {
     
     //합산
     let result = 0;
-    for (let i = 0; i < NUMBERS.length; i++){
-      result += NUMBERS[i];
+    for (let i = 0; i < numbers.length; i++){
+      result += numbers[i];
     }
     Console.print(`결과 : ${result}`) // 테스트파일에 mission-utils의 Console.print만 Spy한다고 되어있음 console.log -> Console.print
   } 
