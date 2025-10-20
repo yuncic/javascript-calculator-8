877039c1dbe01356e8b685bf3708d661729d3f59
김상윤 <ksy151020@gmail.com>
2025-10-21 02:35:19 +0900

    fix: 오타 수정

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import { Console } from '@woowacourse/mission-utils';
 
 class App {
   async run() {
-    const stingInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    const stringInput = await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
 
     //빈 문자열 처리
-    if (stingInput === '') {
+    if (stringInput === '') {
       Console.print('결과 : 0'); // 테스트파일에 mission-utils의 Console.print만 Spy한다고 되어있음 console.log -> Console.print
       return;
     }
 
     // 입력값 시작 검사
-    if (!stingInput || (!stingInput.startsWith('//') && !(Number(stingInput[0]) >= 0))) {
-      if (stingInput.startsWith('-')){
+    if (!stringInput || (!stringInput.startsWith('//') && !(Number(stringInput[0]) >= 0))) {
+      if (stringInput.startsWith('-')){
         throw new Error('[ERROR] 음수는 입력할 수 없습니다.'); //기존의 proccess.exit(1)은 코드를 강제종료 시켜서 에러 throw 하기도 전에 종료됨 -> 테스트 에러
         
       } 
@@ -26,13 +26,13 @@ class App {
     let numberPart = '';
 
     //커스텀 구분자 처리
-    if (stingInput.startsWith('//')) {
+    if (stringInput.startsWith('//')) {
       // 커스텀 구분자 문법 검사
-      if (!stingInput.includes('\\n')){
+      if (!stringInput.includes('\\n')){
         throw new Error('[ERROR] 커스텀 구분자 문법을 제대로 입력해주세요.');
       }
       // '\n' 기준으로 커스텀 구분자와 본문 분리
-      [delimiterPart, numberPart] = stingInput.split('\\n');
+      [delimiterPart, numberPart] = stringInput.split('\\n');
       const CUSTOM_DELIMITER = delimiterPart.replace('//','');
       // 커스텀 구분자로 분리
       numbers = numberPart.split(CUSTOM_DELIMITER).map(str => {
@@ -48,7 +48,7 @@ class App {
           throw new Error("[ERROR] 제대로된 구분자를 사용해주세요");          
         }
 
-        if (NUM <0 ) {
+        if (NUM < 0 ) {
           throw new Error('[ERROR] 음수는 입력할 수 없습니다')
         }
 
@@ -62,7 +62,7 @@ class App {
 
     }else{
       // 기본 구분자
-      numbers = stingInput.split(/,|:/).map(str => {
+      numbers = stringInput.split(/,|:/).map(str => {
         const NUM_STR = str.trim();
         const NUM = Number(NUM_STR);
 
